25d39248768f266db80bf5dbd77e7bfdbe3c2a2f
dr-bcyadav <bankim.97@gmail.com>
2025-01-02 19:07:11 +0530

    Added first file

---
+++ 01_essentials.py
@@ -0,0 +1,36 @@
+# %% ===================================================================== #
+#                                   ABOUT                                  #
+# ======================================================================== #
+# This code cover the essential concepts of Python to achieve high efficiency in coding operations.
+
+
+
+# %% ===================================================================== #
+#                                 LIBRARIES                                #
+# ======================================================================== #
+import pandas as pd 
+
+
+
+# %% ===================================================================== #
+#                                 COMMENTS                                 #
+# ======================================================================== #
+# Note: Python ignores the string literals that are not assigned to a variable. 
+"""So, we can use these string literals as comments, or the usual hash preceded statements."""
+
+
+
+# ======================================================================== #
+#                     MULTIPLE ASSIGNMENT OF VARIABLES                     #
+# ======================================================================== #
+# Assigning same value to multiple variables
+a = b = c = 100
+print(a,b,c)
+
+# Assigning different values to multiple variables 
+a,b,c = 1,2.5,'this_string'
+print(a,b,c)
+
+
+
+
