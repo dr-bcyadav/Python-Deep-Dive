a12fbacdb857f5048fb3c9b5aee251f063a1fe6c
dr-bcyadav <bankim.97@gmail.com>
2025-02-12 11:56:58 +0530

    added control flow keywords

--- 01_essentials.py
+++ 01_essentials.py
@@ -1,7 +1,19 @@
 # %% ===================================================================== #
 #                                   ABOUT                                  #
 # ======================================================================== #
-# This code cover the essential concepts of Python to achieve high efficiency in coding operations.
+# --------------------- Compiled by: (Dr.) B.C. Yadav -------------------- #
+# ------------------------ On: 2025/01/06 19:48:02 ----------------------- #
+# --------------------- Updated: 2025/01/06 19:48:02 --------------------- #
+# ======================================================================== #
+# This code cover the essential concepts of Python to achieve a good efficiency in coding operations.
+
+# TOC:
+    # Libraries
+    # Comments
+    # Multiple assignment of variables
+    # Local and global variables 
+    # Object reference in python
+    # Keywords in python
 
 
 
@@ -122,9 +134,9 @@ y = 'git'
 # We can always delete a variable in python using the `del` keyword. Trying to access a variable deleted from the namespace or an undefined one, both raise `NameError`
 x = 10
 del x 
-print(x)  # deleted variable
+# print(x)  # deleted variable
 
-print(z)  # undefined variable
+# print(z)  # undefined variable
 
 
 
@@ -140,7 +152,7 @@ for word in keyword.kwlist:
 # |---------------------  |-----------------------------------------------|
 # |Category               |Keywords                                       |
 # |---------------------  |-----------------------------------------------|
-# |Value Keywords         |True, False, None                              |
+# |Value Keywords         |True, False, None, del                         |
 # |Operator Keywords      |and, or, not, in, is                           |
 # |Control Flow Keywords  |if, else, elif, for, while, break, continue, pass, try, except, finally, raise, assert                                 |
 # |Function and Class     |def, return, lambda, yield, class              |
@@ -148,4 +160,81 @@ for word in keyword.kwlist:
 # |Import and Module      |import, from, as                               |
 # |Scope and Namespace    |global, nonlocal                               |
 # |Async Programming      |async, await                                   |
+
+
+
+# %% ========================= Value Keywords ============================ #
+# True, False represent boolean values while Null is a special constant used to denote a null value or void.
+print(False == 0)
+print(True == 1)
+
+print(True + True + True + True)
+print(False + False + False + False)
+
+# Null does not equate to zero or any empty data structure
+print(None==0)
+print(None==[])
+
+
+
+# %% ======================== Operator Keywords ========================== #
+# and, or and not 
+a, b = True, False
+print(a and b)  # True when both true 
+print(a or b)  # True when one of them is true 
+print(not a)  # prints the negation of a
+
+# in keyword (membership operator): checks a value in a sequence (list, tuple, string, etc.)
+print('s' in 'this code repository')
+
+# is keyword: checks whether two variables point to the same object in memory 
+# Example 1: 
+print('2' is '2')
+
+# Example 2:
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print(a is b)  # both point to same object
+print(a is c)  # different objects (with same values)
+
+
+
+# %% ====================== Control Flow Keywords ======================== #
+# if elif else: An expression when true passes the control to `if` block, otherwise to the `elif` or `else` block.
+if 4 > 5:
+    print("This way")
+elif 4 < 3:
+    print("That way")
+else:
+    print("No way!")
+    
+# for and while are used to create loops
+for i in range(2):
+    print('consistency')
+
+i=0
+while (i<2):
+    print('consistency is the word')
+    i+=1
+    
+# break keyword is used to break out of the flow of a loop and pass the control to the next statement following immediately after the loop (e.g., an outer loop)
+i=0
+while i<4:
+    i+=1
+    if i==3:
+        break
+    print(i)
+
+# continue keyword skips the current iteration of a loop and goes to its next iteration, and does not end the loop. 
+i=0
+while i<4:
+    i+=1
+    if i==3:
+        continue
+    print(i)
+
+# pass keyword: pass is the null statement in python.
+
 
