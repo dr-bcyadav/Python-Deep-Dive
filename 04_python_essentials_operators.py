28c0d34bc392c0759eee46b021008e1d1373fe67
dr-bcyadav <bankim.97@gmail.com>
2025-02-20 05:28:43 +0530

    added about python operators

---
+++ 04_python_essentials_operators.py
@@ -0,0 +1,42 @@
+# ======================================================================== #
+#                                   ABOUT                                  #
+# ======================================================================== #
+# --------------------- Compiled by: (Dr.) B.C. Yadav -------------------- #
+# ------------------------ On: 2025/02/20 05:12:10 ----------------------- #
+# --------------------- Updated: 2025/02/20 05:12:10 --------------------- #
+# ======================================================================== #
+# This code demonstrates parts of the Python concepts needed to code efficiently, focussing on operators.
+
+
+
+# %% ===================================================================== #
+#                                BACKGROUND                                #
+# ======================================================================== #
+# Types of operators in python 
+
+# | Operators             | Type                    |
+# |-----------------------|-------------------------|
+# | +, -, *, /, %         | Arithmetic operator     |
+# | <, <=, >, >=, ==, !=  | Relational operator     |
+# | AND, OR, NOT          | Logical operator        |
+# | &, \|, <<, >>, ~, ^   | Bitwise operator        |
+# | =, +=, -=, *=, %=     | Assignment operator     |
+
+
+
+# %% ===================================================================== #
+#                           ARITHMETIC OPERATORS                           #
+# ======================================================================== #
+a, b = 15,7
+
+# Addition, Subtraction, Multiplication and Division
+print("Addition:", a+b)
+print("Subtraction:", a-b)
+print("Multiplication:", a*b)
+print("Division:", a/b)
+
+# Floor division - returns the quotient
+print("\nFloor division:", a//b)
+
+# Modulus - returns the remainder
+print("Modulus:", a%b)
