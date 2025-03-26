4e41c970f4fbe26ade7bd1800f8a3e36ea0d2e8f
bcyadav <bankim.97@gmail.com>
2025-03-26 18:06:37 +0530

    renamed files and started work on 05_data_types.py

---
+++ 05_Data_types.py
@@ -0,0 +1,28 @@
+# %% ===================================================================== #
+#                                   ABOUT                                  #
+# ======================================================================== #
+# --------------------- Compiled by: (Dr.) B.C. Yadav -------------------- #
+# ------------------------ On: 2025/03/26 18:03:35 ----------------------- #
+# --------------------- Updated: 2025/03/26 18:03:35 --------------------- #
+# ======================================================================== #
+# This code discusses about the various data types in python.
+
+
+
+# %% ===================================================================== #
+#                                  THEORY                                  #
+# ======================================================================== #
+# Since everything is an object in Python programming, Python data types are classes and variables are instances (objects) of these classes. The following are the standard or built-in data types in Python: 
+# Numeric – int, float, complex
+# Sequence Type – string, list, tuple
+# Mapping Type – dict
+# Boolean – bool
+# Set Type – set, frozenset
+# Binary Types – bytes, bytearray, memoryview
+
+
+
+
+# %% ===================================================================== #
+#                             NUMERIC DATA TYPE                            #
+# ======================================================================== #
