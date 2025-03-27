14bbf44eeeb1f0d8a0fc63ad23147f28212580d6
bcyadav <bankim.97@gmail.com>
2025-03-27 19:03:11 +0530

    Added Sequence data types

--- 05_Data_types.py
+++ 05_Data_types.py
@@ -26,3 +26,38 @@
 # %% ===================================================================== #
 #                             NUMERIC DATA TYPE                            #
 # ======================================================================== #
+# Int, Float and Complex data types 
+a = 10
+print(type(a))
+
+b = 10.5
+print(type(b))
+
+c = 3 + 5j
+print(type(c))
+
+
+
+# %% ===================================================================== #
+#                            SEQUENCE DATA TYPE                            #
+# ======================================================================== #
+# Sequence data types are collection of similar or different data types. 
+
+
+
+# %% ============================= Strings =============================== #
+# Strings are arrays of bytes representing unicode characters. In python, there is no character datatype, rather, a character is a string of length 1.
+# Strings in python can be created using single, double or even triple quotes.
+# Different characters of a string can be accessed via indices.
+
+msg = "Python is amazing"
+print(type(msg))  
+
+# Accessing string characters
+print(msg[3]) 
+print(msg[7]) 
+print(msg[-1])
+
+
+
+# %% ============================== Lists ================================ #
